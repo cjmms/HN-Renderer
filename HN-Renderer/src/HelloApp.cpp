b81d8b46f5cea617113517f82bb83ac48d755500
cjmms <brandon.xyw@gmail.com>
2021-12-24 17:31:54 -0800

    create viewport and scissor throw command buffer

--- HN-Renderer/src/HelloApp.cpp
+++ HN-Renderer/src/HelloApp.cpp
@@ -41,7 +41,8 @@ namespace HN
 
     void HelloTriangleApplication::CreatePipeline()
     {
-        auto pipelineConfig = Pipeline::DefaultPipelineConfigInfo(swapChain->width(), swapChain->height());
+        PipelineConfigInfo pipelineConfig{};
+        Pipeline::DefaultPipelineConfigInfo(pipelineConfig);
         pipelineConfig.renderPass = swapChain->getRenderPass();
         pipelineConfig.pipelineLayout = pipelineLayout;
 
@@ -160,6 +161,17 @@ namespace HN
         renderPassInfo.pClearValues = clearValues.data();
 
         vkCmdBeginRenderPass(commandBuffers[imageIndex], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+        VkViewport viewport{};
+        viewport.x = 0.0f;
+        viewport.y = 0.0f;
+        viewport.width = static_cast<float>(swapChain->getSwapChainExtent().width);
+        viewport.height = static_cast<float>(swapChain->getSwapChainExtent().height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+        VkRect2D scissor{ {0, 0}, swapChain->getSwapChainExtent() };
+        vkCmdSetViewport(commandBuffers[imageIndex], 0, 1, &viewport);
+        vkCmdSetScissor(commandBuffers[imageIndex], 0, 1, &scissor);
 
         pipeline->Bind(commandBuffers[imageIndex]);
         model->Bind(commandBuffers[imageIndex]);
--- HN-Renderer/src/Pipeline.hpp
+++ HN-Renderer/src/Pipeline.hpp
@@ -6,15 +6,19 @@
 namespace HN {
 
 	struct PipelineConfigInfo {
-		VkViewport viewport;
-		VkRect2D scissor;
+		PipelineConfigInfo(const PipelineConfigInfo&) = delete;
+		PipelineConfigInfo& operator=(const PipelineConfigInfo&) = delete;
 		
+		VkPipelineViewportStateCreateInfo viewportInfo;
 		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
 		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
 		VkPipelineMultisampleStateCreateInfo multisampleInfo;
 		VkPipelineColorBlendAttachmentState colorBlendAttachment;
 		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
 		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		std::vector<VkDynamicState> dynamicStateEnables;
+		VkPipelineDynamicStateCreateInfo dynamicStateInfo;
+
 		VkPipelineLayout pipelineLayout = nullptr;
 		VkRenderPass renderPass = nullptr;
 		uint32_t subpass = 0;
@@ -43,7 +47,7 @@ namespace HN {
 		// using width, height. Depth[0, 1]
 		// Scissor: 
 		// Render the whole viewport
-		static PipelineConfigInfo DefaultPipelineConfigInfo(uint32_t width, uint32_t height);
+		static void DefaultPipelineConfigInfo(PipelineConfigInfo& configInfo);
 
 	private:
 		static std::vector<char> readFile(const std::string& filePath);
